fc49be2c90e3742261a5a5cb758e99dd32538c09
Rade58 <bajic.rade2@gmail.com>
2020-01-17 17:31:56 +0100

    set

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # lodash/fp/set
 
-## YOU CAN START WITH src/reducers/list-reducer
+## YOU CAN START WITH src/reducers/lists-reducer
+
+AFTER THAT YOU CAN USE set INSIDE cards-reducer
+
+BUT ON CARD_CREATE ACTION YOU ARE CHANGING BOTH
+
+- YOU ARE CHANGING &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`cards.entities`
+
+BASICALLY YOU ARE ADDING NEW ENTITY (NEW CARD)
+
+- AND YOU ARE ANDING NEW ID (OF THE NEW CARD) IN &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; `cards.ids`
+
+*AND WHEN YOU HAVE MULTIPLE *lodash/fp/set* YOU CAN USE **lodash/fp/pipe**
+
+CHECKOUT TO lodash_pipe TO SEE IMPLEMENTATION OF **pipe**
 
--- src/reducers/cards-reducer.js
+++ src/reducers/cards-reducer.js
@@ -6,12 +6,11 @@ export default (cards = defaultCards, action) => {
 
   if(action.type === CREATE_CARD){
 
-    // YOU NEED TO CREATE AND RETURN NEW OBJECT (WITH NEW entities AND NEW ids)
-
     const cardEntities = cards.entities
     const cardIds = cards.ids
 
     const {cardId, title, description} = action.payload
+
 
     return {
       entities: {...cardEntities, [cardId]: {title, description, id: cardId}},
@@ -20,11 +19,5 @@ export default (cards = defaultCards, action) => {
 
   }
 
-  // CURRENT STATE IS PASSED IF THERE'S NO ACTIONS
   return cards
 }
-
-// ALSO YOUR list-reducer NEEDS TO RETURN NEW lists OBJECT WITH ADDED ID TO HIS cards ARRAY
-// THAT HOLDS ALL THE IDS OS CARDS THAT BELONG TO, MENTIONED LIST 
-
-// YOU WILL NEED listId WHICH IS ON payload TOO (SO YO UCAN ACCESS IT INSIDE list reducer)
