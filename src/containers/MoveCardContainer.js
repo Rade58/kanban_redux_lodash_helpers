1576279f02fa1547cede26217a677e360775de69
Rade58 <bajic.rade2@gmail.com>
2020-01-21 21:03:14 +0100

    meomoization implemented

--- src/containers/MoveCardContainer.js
+++ src/containers/MoveCardContainer.js
@@ -1,19 +1,37 @@
 import {connect} from 'react-redux'
 import MoveCard from '../components/MoveCard'
-
-// IMPORT ACTION CREATOR IN HERE
 import {moveCard} from '../actions/card-actions'
-////////////////////////////
+import { createSelector } from 'reselect' 
+
+
+// IT TAKES THIS FUNCTIOPN AND RUNS IT ANY TIME
+const getListsEntities = state => state.lists.entities
+
+
+// MEMOIZATION FUNCTION, 
+const makeListsArray = createSelector(
+  
+  [getListsEntities],
+
+          //  IT WILL RETURN
+
+  listEntities => {       //    THIS FUNCTION OR CACHED VALUE IF   getListsEntities OUTPUT IS THE SAME AS CACHED OBJECT
+
+    console.log(listEntities)
+
+    return Object.values(listEntities)
+  }
+)
+// CACHED OBJECT YOU'LL GOT WHEN ABOVE (SECOND ARGUMENT) FUNCTION WASS CALLED LAST TIME
+
+// SO IT CHECKS
+
 
 
 const mapStateToProps = (state, ownProps) => {
 
-  const {lists} = state
-
-  // SO BASICALLY I WANT ARRAY
-  const listsArray = Object.values(lists.entities)
-  // AND I WANT TO PASS THAT ARRAY AND USE STUFF FROM HIM INSIDE MoveCard
-
+  // IT IS USED HERE
+  const listsArray = makeListsArray(state)
 
   const {cardId, listId} = ownProps
 
@@ -28,4 +46,3 @@ const mapDispatchToProps = {
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(MoveCard)
-
